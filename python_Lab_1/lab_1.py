1f087ca7d61e97c700da85299cb6e61b1e1e3c21
Nikita <nikokozorro@mail.ru>
2023-02-23 03:02:50 +0300

    addminiCalculator, Finished coding in Python

--- python_Lab_1/lab_1.py
+++ python_Lab_1/lab_1.py
@@ -1,8 +1,43 @@
 print('Helow world')
 
+
 arr = list(range(20))
 arr_anser = list()
 for i in arr:
     if (i % 2 == 0) and (i != 0):
         arr_anser += [i]
 print(arr_anser)
+
+
+def miniCalculatir(numb_1,numb_2,operat):
+    if (operat == "+"):
+        return numb_1 + numb_2
+    if (operat == "-"):
+        return numb_1 - numb_2
+    if (operat == "*"):
+        return numb_1 * numb_2
+    if (operat == "/"):
+        if(numb_2 == 0):
+            return "invalid input, divide by zero"
+        return numb_1 / numb_2
+
+while(True):
+    try:
+        print ("please,enter a number_1")
+        numb_1 = input()
+        numb_1 = float(numb_1)
+        print ("please,enter a number_2")
+        numb_2 = input()
+        numb_2 = float(numb_2)
+        break
+    except  ValueError:
+        print("incorrect data entered,please try again")
+
+print ("please,enter a operator")
+while(True):
+    operat = input()
+    if (operat == "+") or (operat == "-") or (operat == "*") or (operat == "/"):
+        print("anser = ",miniCalculatir(numb_1,numb_2,operat))
+        break
+    else:
+        print("The operator was entered incorrectly, please try again")
