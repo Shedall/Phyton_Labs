2c530cb0573a16b31fb7495069e7252d9b72b541
Nikita <nikokozorro@mail.ru>
2023-03-01 03:36:01 +0300

    codstile

--- python_Lab_1/lab_1.py
+++ python_Lab_1/lab_1.py
@@ -1,32 +1,15 @@
-print('Helow world')
+from function import *
 
+helow_world()
 
-arr = list(range(20))
-arr_anser = list()
-for i in arr:
-    if (i % 2 == 0) and (i != 0):
-        arr_anser += [i]
-print(arr_anser)
+listeven()
 
-
-def miniCalculatir(numb_1,numb_2,operat):
-    if (operat == "+"):
-        return numb_1 + numb_2
-    if (operat == "-"):
-        return numb_1 - numb_2
-    if (operat == "*"):
-        return numb_1 * numb_2
-    if (operat == "/"):
-        if(numb_2 == 0):
-            return "invalid input, divide by zero"
-        return numb_1 / numb_2
-
-while(True):
+while (True):
     try:
-        print ("please,enter a number_1")
+        print("please,enter a number_1")
         numb_1 = input()
         numb_1 = float(numb_1)
-        print ("please,enter a number_2")
+        print("please,enter a number_2")
         numb_2 = input()
         numb_2 = float(numb_2)
         break
